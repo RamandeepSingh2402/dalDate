beecefe565eedfc431a99e2d61085d229619f89b
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2023-02-10 12:47:29 -0400

    ls

---
+++ backend/src/main/java/com/dalDate/DalDateApplication.java
@@ -0,0 +1,13 @@
+package com.dalDate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DalDateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DalDateApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dalDate/DalDateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dalDate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DalDateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
